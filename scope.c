5e02e69c3289c3d6d51c759de559764a3ba27579
Daniel Landsman <daniellandsmanprofessional@gmail.com>
2024-11-02 03:59:32 +0000

    Finished draft of scope module, scope.h and scope.c

--- scope.c
+++ scope.c
@@ -1,18 +1,151 @@
 // Daniel Landsman
 // scope.c: scope file, includes function bodies
 
+#include <stdlib.h>
 #include <string.h>
 #include "scope.h"
 #include "utilities.h"
 
-scope* init_scope(file_location floc, id_kind k, unsigned int ofst_cnt, const char* ident_name, scope* surr_scope)
+// Pre-Conditions: None.
+// Post-Conditions: Returns an empty initialized scope with a size of 0
+// and no associations. Produces error message if space cannot be allocated.
+scope* scope_initialize()
 {
+    // Attempt to allocate space for new scope
     scope* new_scope = (scope*)malloc(sizeof(scope)); // FREE THIS
     if (new_scope == NULL) bail_with_error("No space to allocate scope!");
 
-    strcpy(new_scope->name, ident_name); // NEED TO ALLOCATE MEMORY HERE?
-    new_scope->attrs = create_id_attrs(floc, k, ofst_cnt); // FREE THIS
-    new_scope->parent_scope = surr_scope;
+    // Initialize size and number of associations
+    new_scope->size = 0;
+    new_scope->loc_count = 0;
+    
+    // Initialize all associations to NULL
+    for (int i = 0; i < MAX_SCOPE_SIZE; i++)
+    {
+        new_scope->assoc_arr[i] = NULL;
+    }
 
     return new_scope;
+}
+
+// Pre-Conditions: my_scope is not NULL.
+// Post-Conditions: Returns the number of associations in my_scope.
+address_type scope_loc_count(scope* my_scope)
+{
+    // Check if scope is NULL
+    if (my_scope == NULL)
+    {
+        bail_with_error("Attempted to get number of associations of a NULL scope!");
+    }
+
+    return my_scope->loc_count;
+}
+
+// Pre-Conditions: my_scope is not NULL.
+// Post-Conditions: Returns the current size of my_scope.
+unsigned int scope_size(scope* my_scope)
+{
+    // Check if scope is NULL
+    if (my_scope == NULL)
+    {
+        bail_with_error("Attempted to get size of a NULL scope!");
+    }
+
+    return my_scope->size;
+}
+
+// Pre-Conditions: my_scope is not NULL.
+// Post-Conditions: Returns true if scope is full, false if not full.
+bool scope_full(scope* my_scope)
+{
+    return (scope_size(my_scope) >= MAX_SCOPE_SIZE);
+}
+
+// Pre-Conditions: my_scope and my_name are not NULL.
+// Post-Conditions: Returns true if my_name has an association in my_scope,
+// returns false if my_name has no association in my_scope.
+bool scope_declared(scope* my_scope, const char* my_name)
+{
+    return (scope_lookup(my_scope, my_name) != NULL);
+}
+
+// Pre-Conditions: my_scope, my_name, the associations in the scope,
+// the names of all scope associations, and my_attrs are all not NULL, 
+// my_name has no association in my_scope, my_scope is not full.
+// Post-Conditions: Inserts an association of my_name to my_attrs into
+// my_scope. Produces an error message if space cannot be allocated.
+void scope_insert(scope* my_scope, const char* my_name, id_attrs* my_attrs)
+{
+    // Check if name has an association in scope
+    if (scope_declared(my_scope, my_name))
+    {
+        bail_with_error("An association already exists for (%s)!", my_name);
+    }
+
+    // Check if attributes are NULL
+    if (my_attrs == NULL)
+    {
+        bail_with_error("Attempted to insert an association with NULL attributes!");
+    }
+
+    // Check if scope is full
+    if (scope_full(my_scope))
+    {
+        bail_with_error("Attempted to insert an association into a full scope!");
+    }
+
+    // Attempt to allocate space for new association
+    scope_assoc* new_assoc = (scope_assoc*)malloc(sizeof(scope_assoc)); // FREE THIS
+    if (new_assoc == NULL) bail_with_error("No space to allocate association!");
+
+    // Set name and attribute fields of new association
+    new_assoc->name = my_name;
+    new_assoc->attrs = my_attrs;
+
+    // Set current offset count of association and increment it
+    new_assoc->attrs->offset_count = scope_loc_count(my_scope);
+    my_scope->loc_count++;
+
+    // Set proper index in association array to the new association,
+    // then increment size of scope
+    my_scope->assoc_arr[scope_size(my_scope)] = new_assoc;
+    my_scope->size++;
+}
+
+// Pre-Conditions: my_scope, my_name, the associations in the scope,
+// and the names of all scope associations are not NULL.
+// Post-Conditions: Returns the attributes of the association that my_name has,
+// returns NULL if an association cannot be found for my_name.
+id_attrs* scope_lookup(scope* my_scope, const char* my_name)
+{
+    // Check if name is NULL
+    if (my_name == NULL)
+    {
+        bail_with_error("Attempted to lookup a NULL name!");
+    }
+
+    // Traverse association array to search for name
+    for (int i = 0; i < scope_size(my_scope); i++)
+    {
+        // Check if association is NULL
+        if (my_scope->assoc_arr[i] == NULL)
+        {
+            bail_with_error("Attempting to access a NULL association!");
+        }
+
+        // Check if association's name is NULL
+        if (my_scope->assoc_arr[i]->name == NULL)
+        {
+            bail_with_error("Attempting to access a NULL name!");
+        }
+
+        // Check if current association's name is the name we're looking for
+        if (!strcmp(my_scope->assoc_arr[i]->name, my_name))
+        {
+            return my_scope->assoc_arr[i]->attrs;
+        }
+    }
+
+    // Name not found in the scope
+    return NULL;
 }
--- scope.h
+++ scope.h
@@ -1,13 +1,61 @@
 // Daniel Landsman
 // scope.h: scope header file, includes scope data structure and function declarations
+#ifndef _SCOPE_H
+#define _SCOPE_H
 
+#include <stdbool.h>
 #include "id_attrs.h"
 #include "id_use.h"
 #include "machine_types.h"
 
-typedef struct scope
+#define MAX_SCOPE_SIZE 4096
+
+typedef struct
 {
     const char* name; // Name of identifier
     id_attrs* attrs; // Attributes of identifier
-    struct scope* parent_scope; // Scope immediately surrounding this scope
+} scope_assoc;
+
+typedef struct
+{
+    unsigned int size; // Size of scope
+    unsigned int loc_count; // Number of associations in scope
+    scope_assoc* assoc_arr[MAX_SCOPE_SIZE]; // Array of pointers to scope associations
 } scope;
+
+// Pre-Conditions: None.
+// Post-Conditions: Returns an empty initialized scope with a size of 0
+// and no associations. Produces error message if space cannot be allocated.
+extern scope* scope_initialize();
+
+// Pre-Conditions: my_scope is not NULL.
+// Post-Conditions: Returns the number of associations in my_scope.
+extern address_type scope_loc_count(scope* my_scope);
+
+// Pre-Conditions: my_scope is not NULL.
+// Post-Conditions: Returns the current size of my_scope.
+extern unsigned int scope_size(scope* my_scope);
+
+// Pre-Conditions: my_scope is not NULL.
+// Post-Conditions: Returns true if scope is full, false if not full.
+extern bool scope_full(scope* my_scope);
+
+// Pre-Conditions: my_scope and my_name are not NULL.
+// Post-Conditions: Returns true if my_name has an association in my_scope,
+// returns false if my_name has no association in my_scope.
+extern bool scope_declared(scope* my_scope, const char* my_name);
+
+// Pre-Conditions: my_scope, my_name, the associations in the scope,
+// the names of all scope associations, and my_attrs are all not NULL, 
+// my_name has no association in my_scope, my_scope is not full.
+// Post-Conditions: Inserts an association of my_name to my_attrs into
+// my_scope. Produces an error message if space cannot be allocated.
+extern void scope_insert(scope* my_scope, const char* my_name, id_attrs* my_attrs);
+
+// Pre-Conditions: my_scope, my_name, the associations in the scope,
+// and the names of all scope associations are not NULL.
+// Post-Conditions: Returns the attributes of the association that my_name has,
+// returns NULL if an association cannot be found for my_name.
+extern id_attrs* scope_lookup(scope* my_scope, const char* my_name);
+
+#endif
