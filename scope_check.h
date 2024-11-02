33ae9c4dae5f5f2348871aa7e6646766219ed082
michaeljohncs <michaeljohnua@gmail.com>
2024-11-02 18:16:07 -0400

    Add files via upload

---
+++ scope_check.h
@@ -0,0 +1,13 @@
+#ifndef SCOPE_CHECK_H
+#define SCOPE_CHECK_H
+
+#include "symtab.h"
+#include "ast.h"
+
+// Forward declaration for the function to check program declarations
+void scope_check_program(symtab *st, AST *program_ast);
+
+// Recursive function to check declarations in the AST
+void check_declarations(symtab *st, AST *node);
+
+#endif
